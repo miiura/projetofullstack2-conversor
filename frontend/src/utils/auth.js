6b28463b5352f920948fdb17976fc70e252f2d52
Leda Miura <147261000+miiura@users.noreply.github.com>
2025-12-03 00:26:28 -0300

    cleanup

--- frontend/src/utils/auth.js
+++
@@ -1,9 +0,0 @@
-export function isLogged() {
-  // simple check for a token
-  return !!localStorage.getItem('token');
-}
-
-export function logout() {
-  localStorage.removeItem('token');
-  window.location.href = '/login';
-}
